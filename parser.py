8c4f822d241d3024a84d56afb18543da729e8736
Ustym Hanyk <ustym.hanyk@ucu.edu.ua>
2021-04-17 08:46:52 +0300

    Fixed none-requirements error

--- parser.py
+++ parser.py
@@ -33,20 +33,20 @@ def parse_info():
 
     last_page = int(soup.find('a', title="Go to last page").attrs["href"][-2:])
     while page <= last_page:
-        print(page, page_url)
+        # print(page, page_url)
         page_url = f"https://erasmusintern.org/traineeships?f%5B0%5D=field_traineeship_field_studies%253Aparents_all%3A38&page={page}"
         webpage = requests_session.get(page_url)
         soup = BeautifulSoup(webpage.content, "lxml")
 
         page += 1
 
-        titles_raw = soup.find_all('div', class_="ds-header")  # find names of the review authors
+        titles_raw = soup.find_all('div', class_="ds-header")
         titles = [title.text.strip() for title in titles_raw]
 
-        subtitles_raw = soup.find_all('div', class_="ds-top-content")  # find names of the review authors
+        subtitles_raw = soup.find_all('div', class_="ds-top-content")
         subtitles = [subtitle.text.strip() for subtitle in subtitles_raw]
 
-        recruiters_raw = soup.find_all('div', class_="field-name-recruiter-name")  # find names of the review authors
+        recruiters_raw = soup.find_all('div', class_="field-name-recruiter-name") 
         recruiters = [recruiter.text.strip() for recruiter in recruiters_raw]
 
         locations_raw = soup.find_all('div', class_="field-name-field-traineeship-full-location")
@@ -68,33 +68,25 @@ def parse_info():
         requirements = []
         for full_info_link in full_info_links:
             full_info_webpage = requests_session.get(info_link_base + full_info_link)
+            # print(info_link_base+ full_info_link)
             soup = BeautifulSoup(full_info_webpage.content, "lxml")
             period = soup.find('span', class_="date-display-range").text.strip()
             periods.append(period)
-            requirement = soup.find('div', class_="panel-body").text.strip()
-            requirements.append(requirement)
+
+            requirement = soup.find('div', class_="panel-body")
+            if requirement:
+                requirements.append(requirement.text.strip())
+            else:
+                requirements.append(None)
+
 
         full_info = list(zip(titles, subtitles, recruiters, locations, durations, deadlines, periods, requirements))
         for offer in full_info:
             offer_dict = {"title": offer[0], "subtitle": offer[1], "recruiter": offer[2],
                           "full_location": offer[3], "duration": offer[4], "deadline": offer[5],
                           "periods": offer[6], "requirements": offer[7]}
-        # ic(data_list)
+            data_list.append(offer_dict)
     return data_list
-    # start_time = time.time()
-    # reviews = []
-    # full_review_links = soup.find_all('link',itemprop="url")  # find links to the full reviews
-    # for full_review_link in full_review_links:
-    #     ic(full_review_link.attrs["href"])
-    #     full_review_webpage = requests_session.get(full_review_link.attrs["href"])
-    # soup = BeautifulSoup(full_review_webpage.content, "lxml", parse_only=only_review_tags)
-    #     review_raw_text = soup.find('div', class_="reviewText")  # find full text of the review
-    #     reviews.append(review_raw_text.text.strip())  # add review text to the reviews list
-    #     ic(time.time() - start_time)
-    #     start_time = time.time()
-    # full_reviews = list(zip(names, ratings, reviews)) # make a list of tuples containing full info about reviews
-    # ic(full_reviews)
-    # return full_reviews
-print(parse_info())
+# print(parse_info())
 
 
